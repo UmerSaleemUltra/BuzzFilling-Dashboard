0f469b76e609ea98385a8d513a4185c1427dedf3
Umer Saleem <162819866+UmerSaleemUltra@users.noreply.github.com>
2024-12-31 22:22:18 +0500

    update

--- index.mjs
+++ index.mjs
@@ -19,9 +19,6 @@ app.use('/api/auth/user', authRoutes);
 app.use('/api/auth/admin', adminRoutes);
 
 // Set port and start the server
-const PORT =  3000; // Default to 3000 if not provided
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
+
 
 export default app;
